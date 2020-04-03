253b1756e9a41f9244099f63a99aa55fc4f2077b
Marten Sytema <marten@sytematic.nl>
2020-04-03 15:44:49 +0200

    upgrade cljs version, and bump version to 0.1.13

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject re-dnd "0.1.12"
+(defproject re-dnd "0.1.13"
 
   :description "A configurable drag/drop widget + API for re-frame apps"
   :url "https://github.com/Kah0ona/re-dnd.git"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/clojurescript "1.10.597"]
                  [reagent "0.8.1"]
                  [re-frame "0.10.6"]
                  [com.taoensso/timbre "4.10.0"]
